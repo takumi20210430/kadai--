7e3277b9b0195b131165bc8449e89e5fe80dbcce
takumi20210430 <uniquetime.t@gmail.com>
2021-05-04 11:18:43 +0000

    [Finish]kadai-4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,19 @@
 class RelationshipsController < ApplicationController
+
+  def create
+    follow = current_user.active_relationships.build(followed_id: params[:user_id])
+    follow.save
+    # Relationship.create(followed_id: params[:user_id], follower_id: current_user.id)
+    redirect_back fallback_location: users_path
+  end
+
+  def destroy
+    follow = current_user.active_relationships.find_by(followed_id: params[:user_id])
+    follow.destroy
+    #Relationship.find_by(follower_id: params[:user_id], followed_id: current_user.id).destroy
+    redirect_back fallback_location: users_path
+  end
+
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,17 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+   # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id"
+  has_many :followeds, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attachment :profile_image, destroy: false
+
+
+  def followed_by?(user)
+    passive_relationships.find_by(follower_id: user.id).present?
+  end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
    validates :introduction, length:{maximum: 50}
