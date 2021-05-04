03e28a1ea937f23d16a6bf0973b1db500e00ec04
takumi20210430 <uniquetime.t@gmail.com>
2021-05-04 13:19:05 +0000

    [Start]kadai-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searchs/new'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
